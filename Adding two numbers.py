02437e0565b3ab59afd890c97eeb25faf503102e
ShivR-QA <shivr664@gmail.com>
2024-12-12 15:01:17 +0530

    pythonfile

---
+++ Adding two numbers.py
@@ -0,0 +1,6 @@
+# adding two numbers (1234 ) and (9876)
+
+n1=int(input("Hey ! User, please enter the first number\n"))
+n2=int(input("Hey ! User, please enter the second number\n"))
+Add = (n1+n2)
+print("Total addition of n1 and n2 equals\n" , Add)
